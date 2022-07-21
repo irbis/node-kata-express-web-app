f4aec5015fc7a6b7f2c841b3209e4b1f1470771d
Alexsey Nadtochey <a.nadtochey@gmail.com>
2022-07-21 11:15:32 +0300

    Initial version of simple form was added.
    /simpleform shows how to process HTTP query data. Initial version is about to use GET to send data.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import compression from 'compression'
 
 import { homeRouter } from "./routers/home.js";
 import { messageRouter } from "./routers/message.js";
+import { simpleFormRouter } from "./routers/simpleForm.js";
 
 import { fileURLToPath } from 'url'
 import { dirname, sep } from 'path'
@@ -28,8 +29,9 @@ app.disable('x-powered-by')
 app.set('view engine', 'ejs')
 app.set('views', cfg.dir.views)
 
-app.use('/', homeRouter)
-app.use('/message', messageRouter)
+app.use('/', homeRouter) // home page
+app.use('/message', messageRouter) // message section
+app.use('/simpleform', simpleFormRouter) // simple form section
 
 // static files
 app.use(express.static(cfg.dir.static))
---
+++ routers/simpleForm.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+
+export const simpleFormRouter = Router()
+
+const SECTION_TITLE = 'Simple Form'
+
+simpleFormRouter.get("/", (req, res, next) => {
+    res.render('simpleForm',
+        {
+            title: SECTION_TITLE,
+            data: req.query
+        })
+})
